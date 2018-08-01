83f8b24d71dab90b47a24ef975244ecc4914b6c0
tumit <tumit@live.com>
2018-08-01 15:09:51 +0700

    chore: ignore node_modules

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,15 @@
-function isLeapYear( /* Parameters go here */ ) {
-    // Your code here
+function isLeapYear(year: number): boolean {
+    return (isEvenlyDivisibleBy(year, 4)
+        && (isNotEvenlyDivisibleBy(year, 100) || isEvenlyDivisibleBy(year, 400)));
 }
+
+function isNotEvenlyDivisibleBy(year: number, by: number) {
+    return !isEvenlyDivisibleBy(year, by);
+}
+
+function isEvenlyDivisibleBy(year: number, by: number) {
+    return year % by === 0;
+}
+
 
 export default isLeapYear
