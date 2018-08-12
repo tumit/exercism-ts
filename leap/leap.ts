af29aafcb96aee2ca59be078558dd4f551037356
tumit <tumit@live.com>
2018-08-13 00:02:18 +0700

    refactor: use simple condision

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,14 +1,6 @@
 function isLeapYear(year: number): boolean {
-    return (isEvenlyDivisibleBy(year, 4)
-        && (isNotEvenlyDivisibleBy(year, 100) || isEvenlyDivisibleBy(year, 400)));
-}
-
-function isNotEvenlyDivisibleBy(year: number, by: number) {
-    return !isEvenlyDivisibleBy(year, by);
-}
-
-function isEvenlyDivisibleBy(year: number, by: number) {
-    return year % by === 0;
+    return year % 4 === 0
+        && (year % 100 !== 0 || year % 400 === 0);
 }
 
 export default isLeapYear
